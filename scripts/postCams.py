7540174cf799c05dd20d7d081249fe3ace308216
Andrew T. Brown <andrew7brown@gmail.com>
2017-10-21 16:36:23 -0600

    jpg only

--- scripts/postCams.py
+++ scripts/postCams.py
@@ -5,5 +5,6 @@ with open('cams_update.json') as data:
     json_data = json.load(data)
 
 for item in json_data:
-    requests.post('http://localhost:8080/cameras', json=item)
-    print(json.dumps(item))
+    if ".jpg" in item['url']:
+        requests.post('http://localhost:8080/cameras', json=item)
+        print(json.dumps(item))
